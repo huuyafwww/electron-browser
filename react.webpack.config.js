060d9f5876de2eb17af977accf8724f48975da32
huuyafwww <huuya1234fwww@gmail.com>
2020-07-19 17:49:33 +0900

    Add WebView

--- react.webpack.config.js
+++ react.webpack.config.js
@@ -2,36 +2,6 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/main/js/main.js',
-    output: {
-        filename: 'main.js',
-        path: path.join(
-            __dirname,
-            'public'
-        )
-    },
-    target: 'electron-main',
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-                options: {
-                    fix: true,
-                },
-            }
-        ]
-    },
-    node: {
-        __dirname: false,
-        __filename: false
-    }
-};
-
-module.exports = {
-    mode: 'development',
     entry: './src/renderer/js/app.js',
     output: {
         filename: 'renderer.js',
